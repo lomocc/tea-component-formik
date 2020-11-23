1a0298b892c7cb30dae9beb7b0a8592723d5f4bd
lomocc <constlomo@gmail.com>
2020-11-23 18:13:48 +0800

    refactor

---
+++ src/getStatusProps.ts
@@ -0,0 +1,35 @@
+import { FormControlProps } from '@tencent/tea-component';
+import { FieldMetaProps } from 'formik';
+
+/**
+ *  根据 表单 meta 和 isValidating 返回 Form.Item 的 status 和 message
+ * @param meta
+ * @param isValidating
+ */
+export default function getStatusProps(
+  meta: FieldMetaProps<any>,
+  isValidating: boolean
+) {
+  const status = getStatus(meta, isValidating);
+  return {
+    status,
+    message: status === 'error' && meta.error,
+  };
+}
+
+/**
+ *  根据 表单 meta 和 isValidating 返回  status
+ * @param meta
+ * @param isValidating
+ */
+export function getStatus(meta: FieldMetaProps<any>, isValidating: boolean) {
+  if (isValidating) {
+    return 'validating' as FormControlProps['status'];
+  }
+  if (!meta.touched) {
+    return (null as any) as FormControlProps['status'];
+  }
+  return (typeof meta.error === 'string'
+    ? 'error'
+    : 'success') as FormControlProps['status'];
+}
